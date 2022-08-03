fff4f512a46f4b2a6fb5c1e67717fc368af2e07c
Jônatas Tavares <jonatas.santos@ccc.ufcg.edu.br>
2022-08-03 09:48:43 -0300

    Iniciando projeto

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # SAPO
+
+## Equipe
+
+Catarina Ramalho dos Santos - 121110708
+Jônatas Tavares dos Santos - 121110769
+Lucas Leones Costa dos Santos - 121110281
+
+## Responsabilidades
+
+### Desenvolvimento
+
+Catarina Ramalho dos Santos - 1, 4
+Jônatas Tavares dos Santos - 2, 6
+Lucas Leones Costa dos Santos - 3, 5
+
+### Testes
+
+Catarina Ramalho dos Santos - 3, 5
+Jônatas Tavares dos Santos - 1, 4
+Lucas Leones Costa dos Santos - 2, 6
---
+++ SAPO/src/sapo/Facade.java
@@ -0,0 +1,24 @@
+package sapo;
+
+/**
+ * 
+ * @author Catarina Ramalho dos Santos - 121110708
+ * @author Jônatas Tavares dos Santos - 121110769
+ * @author Lucas Leones Costa dos Santos - 121110281
+ */
+public class Facade {
+
+	// private PessoaController pessoaController;
+	// … demais controllers
+
+	public Facade() {
+		// this.pessoaController = new PessoaController();
+		// … demais controllers
+	}
+
+	public void cadastrarPessoa(String cpf, String nome, String[] habilidades) {
+		// this.pessoaController.cadastrarPessoa(cpf, nome, habilidades);
+	}
+
+	// demais métodos …
+}
