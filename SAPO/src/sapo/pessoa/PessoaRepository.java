37b42d4ff1bc46a1a8b29a81fc986e3f718ddde8
Catarina Ramalho <catarina.santos@ccc.ufcg.edu.br>
2022-08-10 20:35:30 -0300

    Criação do Pessoa Repository

--- SAPO/src/sapo/pessoa/PessoaRepository.java
+++ SAPO/src/sapo/pessoa/PessoaRepository.java
@@ -1,5 +1,25 @@
 package sapo.pessoa;
 
-public class PessoaRepository {
+import java.util.HashMap;
+import java.util.Map;
 
+public class PessoaRepository {
+	Map<String,Pessoa> pessoas;
+	
+	public PessoaRepository() {
+		this.pessoas = new HashMap<String, Pessoa>();
+	}
+	
+	public void cadastrarPessoa(String cpf, String nome, String[] habilidades) {
+		Pessoa pessoa = new Pessoa(cpf,nome,habilidades);
+		this.pessoas.put(cpf, pessoa);
+	}
+	
+	public void removerPessoa(String cpf) {
+		this.pessoas.remove(cpf);
+	}
+	
+	public Pessoa recuperarPessoa(String cpf) {
+		return this.pessoas.get(cpf);
+	}
 }
