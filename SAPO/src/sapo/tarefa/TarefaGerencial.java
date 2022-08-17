b652e87ed0d78634646dd4707c9e1185372208a8
Catarina Ramalho <catarina.santos@ccc.ufcg.edu.br>
2022-08-17 15:51:35 -0300

    Cadastro de TarefaGerencial

--- SAPO/src/sapo/tarefa/TarefaGerencial.java
+++ SAPO/src/sapo/tarefa/TarefaGerencial.java
@@ -1,33 +1,53 @@
 package sapo.tarefa;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class TarefaGerencial extends Tarefa {
-	private List<String> idTarefas;
+	private ArrayList<Tarefa> tarefas;
 
-	public TarefaGerencial(String nome, String codigo, String[] habilidades, String nomeAtividade, String[] idTarefas) {
+	public TarefaGerencial(String nome, String codigo, String[] habilidades, String nomeAtividade,
+			ArrayList<Tarefa> tarefas) {
 		super(nome, codigo, habilidades, nomeAtividade);
-		this.idTarefas = this.converterArrayParaLista(idTarefas);
+		this.tarefas = tarefas;
+	}
+
+	public void adicionarNaTarefaGerencial(Tarefa tarefa) {
+		this.tarefas.add(tarefa);
+	}
+
+	public void removerDaTarefaGerencial(Tarefa tarefa) {
+		this.tarefas.remove(tarefa);
+	}
+
+	public int contarTodasTarefasNaTarefaGerencial() {
+		return this.tarefas.size();
 	}
 	
-	private List<String> converterArrayParaLista(String[] idTarefas) {
-		List<String> lista = new ArrayList<String>();
-		for (String id : idTarefas) {
-			lista.add(id);
+	private String listarTarefas() {
+		String listagem =  "";
+		for (int i = this.tarefas.size(); i > -1; i--) {
+			listagem += this.tarefas.get(i).getNome() + " - " + this.tarefas.get(i).getCodigo() + "\n";;
 		}
-		return lista;
+		return listagem.trim();
 	}
 	
-	 public void adicionarNaTarefaGerencial(String idTarefa) {
-		 this.idTarefas.add(idTarefa);
-	 }
+	@Override
+	public String toString() {
+		String retorno = "";
+		retorno += this.nome + " - " + this.codigo + "\n";
+		retorno += "- " + this.nomeAtividade + "\n";
 
-	 public void removerDaTarefaGerencial(String idTarefa){
-		 this.idTarefas.remove(idTarefa);
-	 }
+		retorno += Arrays.toString(habilidadesRecomendadas).substring(1,
+				(Arrays.toString(habilidadesRecomendadas).length() - 1)) + "\n";
 
-	 public int contarTodasTarefasNaTarefaGerencial() {
-		 return this.idTarefas.size();
-	 }
+		retorno += "(" + this.duracao + " hora(s) executada(s))\n===\nEquipe:\n";
+		for (String cpf : this.pessoasAssociadas.keySet()) {
+			retorno += this.pessoasAssociadas.get(cpf).getNome() + " – " + this.pessoasAssociadas.get(cpf).getCpf();
+		}
+		retorno += "\n===\nTarefas:\n" + this.listarTarefas();
+
+		return retorno;
+
+	}
 }
