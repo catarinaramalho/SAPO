d636882fd46f8f44bff9222fca6ec57c35c70a37
Catarina Ramalho <catarina.santos@ccc.ufcg.edu.br>
2022-08-23 09:07:57 -0300

    Correção de Pessoa

--- SAPO/src/sapo/ValidadorPadrao.java
+++ SAPO/src/sapo/ValidadorPadrao.java
@@ -5,9 +5,9 @@ import java.util.Objects;
 public class ValidadorPadrao {
 
 	public void validacaoHabilidades(String[] habilidades) {
-		Objects.requireNonNull(habilidades, "Habilidades não pode ser nulo!!!");
+		Objects.requireNonNull(habilidades, "Habilidades não pode ser nulo!");
 		for (String habilidade : habilidades) {
-			Objects.requireNonNull(habilidade, "Não pode habilidade nula!!!");
+			Objects.requireNonNull(habilidade, "Não pode habilidade nula!");
 			if (habilidade.isBlank()) {
 				throw new IllegalArgumentException("Não pode habilidade vazia!");
 			}
@@ -15,23 +15,23 @@ public class ValidadorPadrao {
 	}
 
 	public void validacaoNome(String nome) {
-		Objects.requireNonNull(nome, "O nome não pode ser nulo");
+		Objects.requireNonNull(nome, "O nome não pode ser nulo!");
 		if (nome.isBlank()) {
-			throw new IllegalArgumentException("O nome não pode ser vazio");
+			throw new IllegalArgumentException("O nome não pode ser vazio!");
 		}
 	}
 
 	public void validacaoCpf(String cpf) {
-		Objects.requireNonNull(cpf, "O CPF não pode ser nulo");
+		Objects.requireNonNull(cpf, "O CPF não pode ser nulo!");
 		if (cpf.isBlank()) {
-			throw new IllegalArgumentException("O CPF não pode ser vazio");
+			throw new IllegalArgumentException("O CPF não pode ser vazio!");
 		}
 	}
 	
 	public void validacaoId(String id) {
-		Objects.requireNonNull(id, "O id não pode ser nulo");
+		Objects.requireNonNull(id, "O id não pode ser nulo!");
 		if (id.isBlank()) {
-			throw new IllegalArgumentException("O id não pode ser vazio");
+			throw new IllegalArgumentException("O id não pode ser vazio!");
 		}
 	}
 }
--- SAPO/src/sapo/pessoa/Pessoa.java
+++ SAPO/src/sapo/pessoa/Pessoa.java
@@ -10,20 +10,71 @@ import java.util.Objects;
 import sapo.funcao.Funcao;
 import sapo.tarefa.Tarefa;
 
+/**
+ * Representação de uma pessoa. Uma pessoa possui nome, cpf (sua identificação)
+ * e pode conter habilidades e uma função.
+ * 
+ * @author Catarina Ramalho dos Santos - 121110708
+ * @author Jônatas Tavares dos Santos - 121110769
+ * @author Lucas Leones Costa dos Santos - 121110281
+ */
 public class Pessoa implements Comparable<Pessoa> {
+
+	/**
+	 * Identificador da pessoa.
+	 */
 	private String cpf;
+	/**
+	 * Nome da pessoa.
+	 */
 	private String nome;
+	/**
+	 * Habilidades de uma pessoa, que pode ser uma lista vazia de habilidades.
+	 */
 	private String[] habilidades;
+	/**
+	 * Lista de Comentários que uma pessoa pode receber.
+	 */
 	private List<Comentario> comentarios;
+	/**
+	 * Função de uma pesssoa (professor e aluno) ou pode ser nulo.
+	 */
 	private Funcao funcao;
+	/**
+	 * Nível de uma pessoa.
+	 */
 	private int nivel;
+	/**
+	 * Conjunto de tarefas avaliadas de pessoa.
+	 */
 	private Set<Tarefa> tarefasAvaliadas;
+	/**
+	 * O validador dos atributos de pessoa.
+	 */
 	private ValidadorPessoa validador;
 
+	/**
+	 * Construtor de Pessoa. Constrói uma pessoa a partir de um cpf, nome e
+	 * habilidades.
+	 * 
+	 * @param cpf         String com o identificador único da pessoa.
+	 * @param nome        String com o nome da pessoa.
+	 * @param habilidades array contendo a lista de habilidades.
+	 */
 	public Pessoa(String cpf, String nome, String[] habilidades) {
 		this(cpf, nome, habilidades, null);
 	}
 
+	/**
+	 * Construtor de Pessoa. Constrói uma pessoa a partir de um cpf, nome e
+	 * habilidades e uma função, inicializa o nível com 0, o validador e o conjunto
+	 * de tarefas avaliadas.
+	 * 
+	 * @param cpf         String com o identificador único da pessoa.
+	 * @param nome        String com o nome da pessoa.
+	 * @param habilidades array contendo a lista de habilidades.
+	 * @param funcao      Função da pessoa.
+	 */
 	public Pessoa(String cpf, String nome, String[] habilidades, Funcao funcao) {
 		this.cpf = cpf;
 		this.nome = nome;
@@ -87,17 +138,17 @@ public class Pessoa implements Comparable<Pessoa> {
 		Arrays.sort(this.habilidades);
 		String listagemHabilidades = "";
 		for (String habilidade : habilidades) {
-			listagemHabilidades += "- " + habilidade + "\n";
+			listagemHabilidades += "\n- " + habilidade;
 		}
-		return listagemHabilidades.trim();
+		return listagemHabilidades;
 	}
 
 	public String listarComentarios() {
-		String listagemComentarios = this.toString() + "\nComentários:\n";
+		String listagemComentarios = this.toString() + "\nComentários:";
 		for (Comentario comentario : this.comentarios) {
-			listagemComentarios += "-- " + comentario.toString() + "\n";
+			listagemComentarios += "\n-- " + comentario.toString();
 		}
-		return listagemComentarios.trim();
+		return listagemComentarios;
 	}
 
 	public boolean possuiHabilidade(String criterio) {
@@ -116,11 +167,13 @@ public class Pessoa implements Comparable<Pessoa> {
 
 	@Override
 	public String toString() {
-		String toString = this.nome + " - " + this.cpf + "\n";
+		String toString = this.nome + " - " + this.cpf;
 		if (funcao != null) {
-			toString += this.funcao.toString() + "\n";
+			toString += "\n" + this.funcao.toString();
 		}
-		toString += this.listarHabilidades();
+		if (!this.listarHabilidades().isBlank()) {
+			toString += this.listarHabilidades();
+		}
 		return toString;
 	}
 
